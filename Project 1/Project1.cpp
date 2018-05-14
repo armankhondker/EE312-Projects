d5df46cdadeac1595a01c13c9969074cd2328a7f
ArmanKhondker <armankhondker@utexas.edu>
2018-05-14 15:36:38 -0500

    Final Commit

--- Project 1/Project1.cpp
+++ Project 1/Project1.cpp
@@ -1,11 +1,10 @@
-/*
- * Project1.cpp
- * 
- * Name: Arman Khondker
- * Eid : aak2464
- * EE312 Spring 2018
- * SpellCheck
- */
+// SpellCheck <Project1.cpp>
+// EE 312 Project 1 submission by 2/4/18
+// Arman Khondker
+// aak2464
+// Slip days used: <0>
+// Spring 2018
+
 
 #include <stdio.h> // provides declarations for printf and putchar
 #include <stdint.h> // provides declarations for int32_t uint32_t and the other (new) standard C types
@@ -27,10 +26,122 @@
  * almost certainly find it easiest to just write everything you need from scratch!
  */
 
+/*     *****Function isLetter*****
+ * Input: A single char
+ * Outputs: 0 (FALSE) if the char is not a letter
+ *          1 (TRUE) if the char is a letter
+ */
 
-void spellCheck(char article[], char dictionary[]) 
+int inAlphabet(char letter)
+{   if(((letter > 0x60 && letter < 0x7B)||(letter<0x5B && letter > 0x40 ))== 0)  //checks to see if is not in Alphabet
+    {
+        return 0;         //False
+    }
+    else
+    {
+        return 1;         //True
+    }
+}
+
+/*      *****Function printWord*****
+ * Input: A char pointer to a word that is null terminated
+ * Output: Prints the input to console
+ */
+
+void printWord(char* word)
 {
+    printf("%s\n",word);     //prints the word with a new line escape sequence
+}
+
+/*     *****Function inDictionary*****
+ * Inputs: The char pointer to the dictionary, the length of a word, the word that we are checking
+ * Outputs: Prints the word given ONLY if it is not in the dictionary
+ */
+
+void inDictionary(char dictionary[], int length, char* word)
+{
+    int i =0;        //index of dictionary
+    int j= 0;        //index of word
+    int flag =0;          //flag to determine whether or not we print out the word beacuse it is mispelled
+    int dictionaryWordLength=0;
+
+    while(dictionary[i]!=0)
+    {
+        j=0;                                //reset j every time
+        dictionaryWordLength=0;             //reset the comparison dictionary word length every time
+        int actualDictionaryWordLength=0;     /* need to use and increment this variable so that edge case is passed
+                                              this is the actual length of the current word in dictionary */
+
+        while (dictionary[i] != '\n')           //continue until the end of a dictionary word
+        {
+
+            if (dictionary[i] == word[j] || dictionary[i] == word[j] + 0x20 || dictionary[i] == word[j]-0x20)  //checks to see if same letter regardless of uppercase or lowercase
+            {
+                i++;
+                j++;
+                dictionaryWordLength++;
+            }
+            else
+            {
+                i++;             //very important to increment here or else will get stuck
+            }
+
+            actualDictionaryWordLength++;  //need to use and increment this variable so that edge case is passed
 
 
+            if(dictionaryWordLength==length && actualDictionaryWordLength==length)     //if same length, then it is in dictionary
+            {
+                flag = 1;                        //Flag set to indicate it is in the dictionary
+            }
+
+        }
+         i++;
+
+    }
+
+   if(flag==0)                         //this means word is not in dictionary, therefore print it
+    {
+        printWord(word);
+    }
 
 }
+
+void spellCheck(char article[], char dictionary[])
+{
+    while (*article!=0)              //keep looping while the article has still not been terminated
+    {
+        int length = 0;                         //set length to 0 every time
+        char *placeInArticle = article;               //This will hold address the current word
+
+        while (inAlphabet(*article))
+        {
+            length++;                          //keeps incrementing length until it reaches a nonletter
+            article++;
+        }
+
+        while (*article && !inAlphabet(*article)==1)        //increment to next word
+        {
+            article++;                //keeps going until reaches next word in article
+        }
+
+
+        if (length != 1)                                          //allows us to pass by 1 letter words
+        {
+            *(placeInArticle + length) = '\0';                      //add null termination so that we can use %s in the printinDictionary(dictionary, length, placeInArticle);          //checks if current word is in dictionary or not, if not then it prints it out
+            inDictionary(dictionary, length, placeInArticle); //checks if current word is in dictionary or not, if not then it prints it out
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Project 2/Project2.cpp
+++ Project 2/Project2.cpp
@@ -1,10 +1,13 @@
-/* Project2.cpp
- * Ima Student
- * EE312, Summer 2017
- * Project 2, Matrix Multiplication
- */
+// matrix-multiplication <Project2.cpp>
+// EE 312 Project 2 submission by
+// Arman Khondker
+// aak2464
+// Slip days used: 0
+// Spring 2018
+// Include the contents of this file at the top of every file that you submit, replacing the fields within <>.  Do not keep the <>.
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
 #include "MatrixMultiply.h"
 
@@ -14,8 +17,36 @@ void multiplyMatrices(
         const uint32_t a_cols,
         double b[],
         const uint32_t b_cols,
-        double c[]) {
-    // https://en.wikipedia.org/wiki/Row-_and_column-major_order
+        double c[])
+{
+
+    const uint32_t b_rows = a_cols;             //columns in A must equal rows in B for multiplication to be valid
+
+    int row =0;
+
+    while( row < a_rows)
+    {
+        int column=0;
+
+        while (column < b_cols)
+        {
+            double sum = 0;
+            int k=0;           //k-loop
+
+            while( k < b_rows)
+            {
+                sum = sum + ( a[(row * a_cols) + k] * b[(k * b_cols) + column] );          //sum will hold the value needed to be stored after looping
+                k++;
+
+            }
+
+            c[row * b_cols + column] = sum;          //set the c matrix to appropriate value
+            column++;
+        }
+
+        row++;
+    }
+
 }
 
 double** multiplyMatricesPtr(
@@ -23,14 +54,68 @@ double** multiplyMatricesPtr(
         const uint32_t a_rows,
         const uint32_t a_cols,
         double** b,
-        const uint32_t b_cols) {
-    return NULL;
+        const uint32_t b_cols)
+{
+
+    const uint32_t b_rows = a_cols;             //columns in A must equal rows in B for multiplication to be valid
+    int z;
+    double** matrix = (double**)malloc(a_rows* sizeof(double*));                     //dynamically allocate the matrix
+    for(z=0; z < a_rows; z++){
+        double* col = (double*)malloc(a_cols* sizeof(double));
+        matrix[z] =col;
+    }
+
+    int row =0;
+
+    while( row < a_rows)
+    {
+        int column=0;
+
+        while (column < b_cols)
+        {
+            double sum = 0;
+            int k=0;                    //k-loop
+
+            while( k < b_rows)
+            {
+                sum = sum + (a[row][k] * b[k][column]);
+                k++;
+
+            }
+            matrix[row][column]=sum;        //set the c matrix to appropriate value
+            column++;
+        }
+        row++;
+    }
+  return matrix;
+
 }
 
-// https://en.wikipedia.org/wiki/Transpose
 double** transposeMatrix(
          double** a,
          const uint32_t a_rows,
-         const uint32_t a_cols) {
-    return NULL;
+         const uint32_t a_cols)
+{
+
+    int z;
+    double** transposeMatrix = (double**)malloc(a_cols* sizeof(double*));              //dynamically allocate the transpose
+    for(z=0; z < a_cols; z++){
+        double* col = (double*)malloc(a_rows* sizeof(double));
+        transposeMatrix[z] =col;
+    }
+
+    int row =0;
+
+    for(row=0; row< a_rows; row++)
+    {
+         int column=0;
+
+         for(column=0; column< a_cols; column++)
+        {
+            transposeMatrix[column][row] = a[row][column];            //creates the transpose
+        }
+
+    }
+
+    return transposeMatrix;
 }
